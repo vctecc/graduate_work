8d1e13147f0fd88445e5e1e3545fcebcefc07a16
Natalia <maltseva@arrival.com>
2021-10-20 14:08:32 +0300

    #24 subscriptions admin panel

--- subscription_admin/subscriptions/admin.py
+++ subscription_admin/subscriptions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your schemas here.
+from .models import *
+
+admin.site.register(Subscription)
+admin.site.register(Product)
--- subscription_admin/subscriptions/models.py
+++ subscription_admin/subscriptions/models.py
@@ -1,3 +1,54 @@
+import uuid
+
 from django.db import models
 
-# Create your schemas here.
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    period = models.IntegerField()
+    price = models.IntegerField()
+    currency_code = models.CharField(max_length=3, default='RUB')
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        managed = False
+        db_table = 'product'
+
+    def get_period(self):
+        return self.period
+
+    def __str__(self):
+        if self.is_active:
+            status = "ACTIVE"
+        else:
+            status = "INACTIVE"
+        return f"[{status}] {self.name} for {self.period} days at the price of {self.price} {self.currency_code}"
+
+
+STATES = (
+    ('ACTIVE', 'active'),
+    ('INACTIVE', 'inactive'),
+    ('CANCELLED', 'cancelled')
+)
+
+
+class Subscription(BaseModel):
+    user_id = models.UUIDField()
+    product = models.ForeignKey(Product, models.DO_NOTHING)
+    state = models.CharField(max_length=255, choices=STATES)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(default=start_date)
+
+    class Meta:
+        managed = False
+        db_table = 'subscription'
