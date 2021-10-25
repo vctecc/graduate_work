bad8ea7fef26c56613b87be1f2d16ec21e2b5524
Natalia <maltseva@arrival.com>
2021-10-25 15:42:05 +0300

    Added queue for registrations, code refactoring

--- order_service/services/payment.py
+++ order_service/services/payment.py
@@ -1,19 +1,21 @@
+import json
 import logging
 from urllib.parse import urljoin
 
 import requests
 
 from models.subscription import Subscription
+from core.config import PaymentsSettings
 
 
 class PaymentService:
-    def __init__(self, url):
-        self.base_api_url = url
+    def __init__(self, settings: PaymentsSettings):
+        self.settings = settings
 
     def register_payment(self, payment: Subscription):
         logging.info(f"Registering payment: {payment}")
-        data = {"user_id": payment.user_id, "price": payment.product.price}
-        response = requests.post(urljoin(self.base_api_url, 'payments/register'), data=data)
+        data = {"user_id": payment.user_id, "amount": str(payment.product.price), "product_id": payment.product.id, "currency": payment.product.currency_code}
+        response = requests.post(urljoin(self.settings.url, 'v1/payments'), data=json.dumps(data))
         response.raise_for_status()
         logging.info("finished registering")
         return
